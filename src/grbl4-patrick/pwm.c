c24fdeb111a02f593ae0c9aa92265147ddcd7f4c
Juergen Weigert <jw@suse.de>
2013-03-23 14:18:06 +0100

    comments added

--- src/grbl4-patrick/pwm.c
+++ src/grbl4-patrick/pwm.c
@@ -1,10 +1,10 @@
 #include <avr/io.h>
 #define OC0A_BIT   3
-#define OC0A_DDR  PORTB
+#define OC0A_DDR  DDRB
 
 void pwm_init(){
 	OC0A_DDR|=1<<OC0A_BIT;
 	TCCR0A=0x83;	// clear OC0A on compare; Fast PWM 3
 	TCCR0B=0x05;	// prescale 1/1024
-	OCR0A=0;
+	OCR0A=100;
 }
